411bc79e5ab3859bec7d4f9b2154e3b7e8bcb27a
Manish <manish.kumar731@gmail.com>
2020-05-24 11:09:22 +0530

    Final commit

---
+++ src/test/java/com/example/manish/SpringBootCloudConfigServer/SpringBootCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manish.SpringBootCloudConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
